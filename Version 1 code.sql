b151ef03ce5fa01baec1d75b2b3d3b128294efeb
Abubakar Rasheed <131538521+abubakarrasheed@users.noreply.github.com>
2024-05-02 03:37:26 -0700

    Update Version 1 code.sql

--- Version 1 code.sql
+++ Version 1 code.sql
@@ -1,20 +1,31 @@
 create database classicmodels;
 use classicmodels;
+-- Q 1
 
 select * from employees
 where firstName like '_____%';
 
+-- Q 2
+
+
 select * from products 
 where quantityInStock > 4000;
+
+-- Q 3
+
 
 select *
 from employees
 where jobTitle='Sales Rep';
 
+-- Q 4
+
 select customerName
 from customers
  where country IN ('France', 'Norway', 'Denmark');
- 
+
+-- Q 5
+
  select customerNumber , customerName , phone
  from customers
  where country!= 'USA';
